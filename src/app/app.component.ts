d96a4e9b3a42cf0991477cf11ff8b9ff9318e8de
Ejaz Ahmed <ejazahmed@pop-os.localdomain>
2020-02-17 08:36:48 +0500

    Password generator completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,44 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'pw';
+  password = '';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  onButtonClick() {
+    const numbers = '1234567890'
+    const letters = 'abcdefgijklmnopqrstuvwxyz'
+    const symbols = '!@$%^&*()'
+
+    let validChars = ''
+    if (this.includeLetters) validChars += letters
+    if (this.includeNumbers) validChars += numbers
+    if (this.includeSymbols) validChars += symbols
+
+    let generatedPassword = ''
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index]
+    } 
+    this.password = generatedPassword;
+  }
+  onInputLength(value: string) {
+    const parsedValue = parseInt(value)
+    if(!isNaN(parsedValue)) {
+      this.length = parsedValue
+      console.log(this.length)
+    }
+  }
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols
+  }
 }
